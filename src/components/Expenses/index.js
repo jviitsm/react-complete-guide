ffccde198e8fb6d3c97ece23d5ffbc5444d647d5
jviitsm <jvictorgmodesto@gmail.com>
2021-09-06 23:41:07 -0300

    filter logic added

--- src/components/Expenses/index.js
+++ src/components/Expenses/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import ExpensesFilter from '../ExpensesFilter';
 import ExpenseItem from '../ExpenseItem';
@@ -9,19 +9,28 @@ import './styles.css';
 export default function Expenses(props) {
 
     const [selectedYear, setSelectedYear] = useState('2021');
+    const [filteredExpenses, setFilteredExpenses] = useState(
+        props.expenses.filter(item => item.date.getFullYear().toString() === selectedYear)
+    );
 
 
     const handleFilterChange = (year) => {
         console.log(year);
         setSelectedYear(year);
+        setFilteredExpenses(props.expenses.filter(item => item.date.getFullYear().toString() === year));
     }
+
+    useEffect(() => {
+        setFilteredExpenses(props.expenses.filter(item => item.date.getFullYear().toString() === selectedYear));
+    }, [props.expenses, selectedYear])
+
 
     return (
         <Card className="expenses">
             <ExpensesFilter selectFilter={handleFilterChange} selectedYear={selectedYear} />
-            {props.expenses.length > 0 ? props.expenses.map(item =>
-                <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />) 
-                : <h2 style={{color: 'white', textAlign: 'center'}}>Nenhuma despesa salva</h2>}
+            {filteredExpenses.length > 0 ? filteredExpenses.map(item =>
+                <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />)
+                : <h2 style={{ color: 'white', textAlign: 'center' }}>Nenhuma despesa salva</h2>}
 
         </Card>
     )
